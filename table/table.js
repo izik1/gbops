24d28cca258a1c1ed11a718327bf7a0988f8a6f4
Zachery Gyutkovitz <zgyurkovitz@gmail.com>
2018-10-21 17:00:42 -0700

    Refactoring :broom:

--- table/table.js
+++ table/table.js
@@ -1,19 +1,10 @@
+'use strict';
+
 var cycle_mode = null;
 
 var width = null;
 
 var tables = [];
-
-group_colors = {
-    "x16/lsm": "MediumOrchid",
-    "x16/alu": "LimeGreen",
-    "x8/rsb": "Cyan",
-    "x8/alu": "Lime",
-    "control/misc": "Tomato",
-    "x8/lsm": "Orchid",
-    "control/br": "LightSalmon",
-    "unused": "SlateGray"
-};
 
 function table_width_changed(event) {
     width = event.target.value;
@@ -26,97 +17,85 @@ function cycle_mode_changed(event) {
 }
 
 function create_op(op) {
-    if(op.Name != "UNUSED")
-        span = $("<span/>").html(`${op.Name}<br/>
-            ${op.Length} ${op_tmpl_helpers.timing(op.TCyclesMin, op.TCyclesMax)}<br>
-            ${op.Flags.Z}&#8203;${op.Flags.N}&#8203;${op.Flags.H}&#8203;${op.Flags.C}`);
-    else span = null;
-    td = $("<td>/").append(span).css("background-color", op_tmpl_helpers.color(op.Group));
-    if(op.TimingMax !== undefined && op.Name != "UNUSED") {
-        td.dblclick(op, enableFloatingBox);
+    let span = null
+    if(op.Name != "UNUSED") {
+        span = $("<pre/>").html(`${op.Name}\n` +
+            `${op.Length} ${op_timing(op.TCyclesMin, op.TCyclesMax, cycle_mode)}\n` +
+            `${op.Flags.Z}&#8203;${op.Flags.N}&#8203;${op.Flags.H}&#8203;${op.Flags.C}`);
     }
 
-    return td; 
+    return $("<td>/").append(span).addClass("opcode").addClass(op.Group);
 }
 
-var table_tmpl_helper = {
-    get_top_header: width => {
-        var row = $('<tr><th>--</th></tr>')
-        for (var i = 0; i < width; i++) {
-            row.append($('<th/>').html('+' + i.toString(16).toUpperCase()));
-        }
 
-        return row[0].outerHTML;
-    },
-
-    get_rows: (step, table_data) => {
-        var get_prefix = row => (row * step).toString(16).toUpperCase().padStart(2, '0') + '+';
-        var rows = $('<div/>');
-        for (var row_num = 0; row_num < 0x100 / step; row_num++) {
-            var row = $('<tr/>').append(`<th>${get_prefix(row_num)}</th>`).appendTo(rows);
-            for (var i = 0; i < step; i++) {
-                current = table_data[row_num * step + i];
-                row.append(create_op(current));
-            }
-        }
-
-        return rows;
+function get_top_header(width) {
+    const row = $('<tr><th>--</th></tr>')
+    for (let i = 0; i < width; i++) {
+        row.append($('<th/>').html('+' + i.toString(16).toUpperCase()));
     }
-};
 
-var op_tmpl_helpers = {
-    timing: (min, max) => {
-        switch (cycle_mode) {
-            case "t": return min !== max ? `${min}t-${max}t` : `${min}t`;
-            case "m": return min !== max ? `${min / 4}m-${max / 4}m` : `${min / 4}m`;
-            case "both":
-            default: return min !== max ? `${min}t‑${max}t&#8203;/&#8203;${min / 4}m‑${max / 4}m` : `${min}t&#8203;/&#8203;${min / 4}m`; // &#8203; is a non-breaking space.
+    return row;
+}
+
+ function get_rows(step, table_data) {
+    const get_prefix = row => (row * step).toString(16).toUpperCase().padStart(2, '0') + '+';
+    const rows = $('<div/>');
+
+    for (let row_num = 0; row_num < 0x100 / step; row_num++) {
+        const row = $('<tr/>').append(`<th>${get_prefix(row_num)}</th>`).appendTo(rows);
+
+        for (let i = 0; i < step; i++) {
+            row.append(create_op(table_data[row_num * step + i]));
         }
-    },
+    }
 
-    color: group => group_colors[group] ? group_colors[group] : "inherit"
-};
+    return rows;
+}
+
+function op_timing(min, max, mode) {
+    switch (mode) {
+        case "t": return min !== max ? `${min}t-${max}t` : `${min}t`;
+        case "m": return min !== max ? `${min / 4}m-${max / 4}m` : `${min / 4}m`;
+        case "both": // &#8203; is a 0 width space.
+        default: return op_timing(min, max, 't') + "&#8203;/&#8203;" + op_timing(min, max, 'm');
+    }
+}
 
 function redrawTables() {
-    var old_unprefixed = $('#unprefixed');
-    var old_cbprefixed = $('#cbprefixed');
-
-    var new_unprefixed = loadTable('unprefixed');
-    var new_cbprefixed = loadTable('cbprefixed');
+    let new_unprefixed = loadTable('unprefixed');
+    let new_cbprefixed = loadTable('cbprefixed');
 
     if (new_unprefixed && new_cbprefixed) {
-        old_unprefixed.remove();
-        old_cbprefixed.remove();
-        $('body').append(new_unprefixed).append(new_cbprefixed);
+        $('table.opcode').hide();
+        new_unprefixed.show();
+        new_cbprefixed.show();
         return;
     }
 
     $.getJSON("dmgops.json", null, ops => {
         if (!new_unprefixed) new_unprefixed = loadTable('unprefixed', ops.Unprefixed);
         if (!new_cbprefixed) new_cbprefixed = loadTable('cbprefixed', ops.CBPrefixed);
-        old_unprefixed.remove();
-        old_cbprefixed.remove();
-        $('body').append(new_unprefixed)
-            .append(new_cbprefixed);
+        $('table.opcode').hide();
+        $('body').append(new_unprefixed).append(new_cbprefixed);
     });
 }
 
 function loadTable(id, table) {
-    if (!tables[width]) tables[width] = [];
-    if (!tables[width][id + '_' + cycle_mode]) {
-        if (!table) return null;
-        table_tmp = $('<table/>').attr('id', id).append(table_tmpl_helper.get_top_header(width)).append(table_tmpl_helper.get_rows(width, table).children())
-        tables[width][id + '_' + cycle_mode] = table_tmp;
-    }
-    
-    return tables[width][id + '_' + cycle_mode].attr('id', id);
+    const table_tmp = $(`#${id}-${width}-${cycle_mode}`);
+
+    if (table_tmp.length > 0) return table_tmp;
+    if (!table) return null;
+    return $('<table/>')
+        .attr('id', `${id}-${width}-${cycle_mode}`)
+        .addClass('opcode')
+        .append(get_top_header(width))
+        .append(get_rows(width, table).children())
+        .on("dblclick", "td.opcode", table, enableFloatingBox);
 }
 
 function init() {
     function bind_get(name, fn) {
-        var v = $(`select[name="${name}"]`);
-        v.on('change', fn);
-        return v.find(':selected').val();
+        return $(`select[name="${name}"]`).on('change', fn).find(':selected').val();
     }
 
     $('#floating-box').click((e) => e.stopPropagation());
@@ -126,15 +105,23 @@ function init() {
 }
 
 function enableFloatingBox(event, cell, supportBubbling) {
-    if (supportBubbling || event.target === event.currentTarget) {
-        cell = event.data;
-        $('#floating-box').html("");
+    if (supportBubbling || event.target === this) {
+        const target = $(event.target);
 
-        for(i = 0; i < cell.TimingMax.length; i++) {
-            $('#floating-box').append(cell.TimingMax[i].Type + "<br/>");
-            if(cell.TimingMax[i].Comment !== "") {
-                $('#floating-box').append(cell.TimingMax[i].Comment + "<br/>");
-            }
+        // kind of a hack, calculate the 2-dim index if the cell.
+        const x = target.index() - 1;
+        const width = target.parent().children().length - 1;
+        const y = target.parent().index() - 1;
+
+        const cell = event.data[y * width + x];
+
+        if(!cell.TimingMax) return;
+
+        const floating_box = $('#floating-box');
+        floating_box.html("");
+        for (let timing_point of cell.TimingMax) {
+            floating_box.append(timing_point.Type + "<br/>");
+            if(timing_point.Comment) floating_box.append(timing_point.Comment + "<br/>");
         }
 
         $('#floating-box-container').show();
