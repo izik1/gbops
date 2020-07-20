13eac592a4ab7a42e64f1ad4a1bbb19e8fa1fe1d
Chloe Ross <orangesnowfox@gmail.com>
2020-07-20 09:44:50 -0700

    Add a very clunky but still somewhat functioning search bar

--- table/table.js
+++ table/table.js
@@ -4,7 +4,7 @@ var cycle_mode = null;
 
 var width = null;
 
-var tables = [];
+var tables = null;
 
 var macOS = false;
 
@@ -20,7 +20,7 @@ function create_op(op) {
 function get_top_header(width) {
     const row = $('<tr><th>--</th></tr>')
     for (let i = 0; i < width; i++) {
-        row.append($('<th/>').html('+' + i.toString(16).toUpperCase()));
+        row.append($('<th/>').html('+' + i.toString(16).toUpperCase()).addClass("header"));
     }
 
     return row;
@@ -60,25 +60,19 @@ function redrawTables() {
     let new_cbprefixed = loadTable('cbprefixed');
 
     if (new_unprefixed && new_cbprefixed) {
+        $('input[name="search_box"]').trigger('keyup');
         $('table.opcode').hide();
         new_unprefixed.show();
         new_cbprefixed.show();
         return;
     }
 
-    // Don't use `getJson()` because it complains locally about the mimetype.
-    $.ajax({
-        dataType: "json",
-        url: "dmgops.json",
-        data: null,
-        mimeType: "application/json",
-        success: ops => {
-            if (!new_unprefixed) new_unprefixed = loadTable('unprefixed', ops.Unprefixed);
-            if (!new_cbprefixed) new_cbprefixed = loadTable('cbprefixed', ops.CBPrefixed);
-            $('table.opcode').hide();
-            $('body').append(new_unprefixed).append(new_cbprefixed);
-        }
-    })
+    if (!new_unprefixed) new_unprefixed = loadTable('unprefixed', tables.Unprefixed);
+    if (!new_cbprefixed) new_cbprefixed = loadTable('cbprefixed', tables.CBPrefixed);
+
+    $('table.opcode').hide();
+    $('body').append(new_unprefixed).append(new_cbprefixed);
+    $('input[name="search_box"]').trigger('keyup');
 }
 
 function loadTable(id, table) {
@@ -136,6 +130,214 @@ function disableFloatingBox() {
     $('#floating-box-container').hide();
 }
 
+function parseIntFromPrefixedString(num) {
+    let radix = 10;
+    if (num.length > 2) {
+        switch (num[1].toUpperCase()) {
+            case "X": radix = 16;
+            case "O": radix = 8;
+            case "B": radix = 2;
+        }
+    }
+
+    return parseInt(radix !== 10 ? num.substr(2) : num, radix);
+}
+
+class ParsedSearch {
+    constructor(ty, val) {
+        this.ty = ty;
+        this.val = val;
+    }
+
+    push(val) {
+        switch (this.ty) {
+            case "keyword":
+                this.val.right.push(val);
+                break;
+            case "block":
+                this.val.push(val);
+                break;
+            case "operator":
+                if (this.val.right !== null) throw new Error("operator::right !== null");
+                this.val.right = val;
+                break;
+            default: throw new TypeError();
+        }
+    }
+
+    pop() {
+        switch (this.ty) {
+            case "keyword": return this.val.right.pop();
+            case "block": return this.val.pop();
+            default: throw new TypeError();
+        }
+    }
+
+    get length() {
+        switch (this.ty) {
+            case "keyword": return this.val.right.length;
+            case "block": return this.val.length;
+            default: throw new TypeError();
+        }
+    }
+
+    exec(opcode, opcodeNumber) {
+        switch (this.ty) {
+            case "block":
+                for (let node of this.val) {
+                    if (!node.exec(opcode, opcodeNumber)) return false;
+                }
+
+                return true;
+            case "keyword": {
+                if (this.val.val === "and") {
+                    return this.val.left.exec(opcode, opcodeNumber) && this.val.right.exec(opcode, opcodeNumber);
+                }
+
+                if (this.val.val === "or") {
+                    return this.val.left.exec(opcode, opcodeNumber) || this.val.right.exec(opcode, opcodeNumber);
+                }
+
+                throw new Error("error made it past validation!!!");
+            }
+
+            case "operator": {
+                const lhs = this.val.left.exec(opcode, opcodeNumber);
+                const rhs = this.val.right.exec(opcode, opcodeNumber);
+                switch (this.val.val) {
+                    case "<": return lhs < rhs;
+                    case "<=": return lhs <= rhs;
+                    case "=": return this.val.right.ty == "str" ? lhs.includes(rhs) : lhs === rhs;
+                    case ">=": return lhs >= rhs;
+                    case ">": return lhs > rhs;
+                }
+            }
+
+            case "number": return parseIntFromPrefixedString(this.val);
+            case "str": return this.val;
+            case "prop": {
+                switch (this.val) {
+                    case "number":
+                    case "num":
+                    case "opcode":
+                    case "op":
+                    case "#":
+                        return opcodeNumber;
+                    case "name": return opcode.Name !== "UNUSED" ? opcode.Name : "";
+                    case "length":
+                    case "len": return opcode.Name !== "UNUSED" ? opcode.Length : null;
+                    default: throw new Error(`unregistered prop: ${this.val}`);
+                }
+            }
+
+            default: throw new Error(`unexpected ty: "${this.ty}"`);
+        }
+    }
+}
+
+function parseOpcodeSearch(str) {
+    let strRegex = /^"([^"]*)"/;
+    let keywordRegex = /^(and|or)\b/;
+    let operatorRegex = /^(<=|>=|=|<|>)/;
+    let propRegex = /^\.((?:number|num|name|opcode|op|length|len)\b|#)/;
+    let numberRegex = /^(0x[\da-f]{1,2}|0o[0-8]{1,3}|(?:0b[01]{1,8})|(?:[1-9]\d{0,2}|\d(?!\d)))\b/i;
+    let result = {
+        r: new ParsedSearch("block", []), next_op: null, push: function (val) {
+            (this.next_op || this.r).push(val);
+            this.next_op && this.r.push(this.next_op);
+            this.next_op = null;
+        }
+    };
+
+    let i = 0;
+
+    function lexOpcodeSearch(offset) {
+        let strMatches = strRegex.exec(str.substr(offset));
+        if (strMatches) {
+            result.push(new ParsedSearch("str", strMatches[1]));
+            return strMatches[0].length;
+        }
+
+        let keywordMatches = keywordRegex.exec(str.substr(offset));
+        if (keywordMatches) {
+            result.r = new ParsedSearch("keyword", { val: keywordMatches[1], left: result.r, right: new ParsedSearch("block", []) })
+            return keywordMatches[0].length;
+        }
+
+        let operatorMatches = operatorRegex.exec(str.substr(offset));
+        if (operatorMatches) {
+            if (result.r.length == 0 || result.next_op !== null) throw new Error();
+
+            result.next_op = new ParsedSearch("operator", { val: operatorMatches[1], left: result.r.pop(), right: null });
+            return operatorMatches[0].length;
+        }
+
+        let propMatches = propRegex.exec(str.substr(offset));
+        if (propMatches) {
+            result.push(new ParsedSearch("prop", propMatches[1]));
+            return propMatches[0].length;
+        }
+
+        let numberMatches = numberRegex.exec(str.substr(offset));
+        if (numberMatches) {
+            result.push(new ParsedSearch("number", numberMatches[1]));
+            return numberMatches[0].length;
+        }
+
+        if (str[offset] === " ") {
+            return 1;
+        }
+    }
+
+    while (i < str.length) {
+        i += lexOpcodeSearch(i);
+    }
+
+    return result.r;
+}
+
+function runOpcodeSearch(str, opcode, opcodeNumber) {
+    let parsed = parseOpcodeSearch(str);
+    if (!validateOpcodeSearch(parsed)) {
+        return true;
+    }
+
+    return parsed.exec(opcode, opcodeNumber);
+}
+
+function propSupportsCompare(prop, compare, rhs) {
+    switch (prop.val) {
+        case "name": return ['<', '<=', '=', '=>', '>'].indexOf(compare) > 0 && rhs.ty === "str";
+        case "#":
+        case "num":
+        case "number":
+        case "opcode":
+        case "op":
+        case "len":
+        case "length":
+            return ['<', '<=', '=', '=>', '>'].indexOf(compare) > 0 && rhs.ty === "number";
+        default: throw new Error(`unexpected prop .${prop.val}`);
+    }
+}
+
+function validateOpcodeSearch(parsed) {
+    switch (parsed.ty) {
+        case "keyword": return (validateOpcodeSearch(parsed.val.left) && validateOpcodeSearch(parsed.val.right))
+        case "operator": return (parsed.val.left.ty === "prop" && propSupportsCompare(parsed.val.left, parsed.val.val, parsed.val.right));
+        case "block": {
+            for (let node of parsed.val) {
+                if (!validateOpcodeSearch(node)) return false;
+            }
+
+            return true;
+        }
+
+        case "prop": return true; // happens in situations where a prop is still being typed like `.len=2 .name or .name="LD"`
+
+        default: throw new Error(`unexpected ty in validation: ${parsed.ty}`);
+    }
+}
+
 $(document).ready(() => {
     function bind_get(name, fn) {
         return $(`select[name="${name}"]`).on('change', e => {
@@ -148,5 +350,30 @@ $(document).ready(() => {
     width = bind_get("table_width", e => width = e.target.value);
     cycle_mode = bind_get("cycle_mode", e => cycle_mode = e.target.value);
     macOS = navigator.appVersion.indexOf("Mac") != -1;
-    redrawTables();
+
+    $('input[name="search-box"]').on('keyup', e => {
+        if (!tables) return;
+        for (let i = 0; i < 0x100; i++) {
+            let unprefixedNode = $($(`#unprefixed-${width}-${cycle_mode}`).children()[Math.floor((i / width) + 1)].children[(i % width) + 1]);
+            let CBPrefixedNode = $($(`#cbprefixed-${width}-${cycle_mode}`).children()[Math.floor((i / width) + 1)].children[(i % width) + 1]);
+
+            if (runOpcodeSearch(e.target.value, tables.Unprefixed[i], i)) unprefixedNode.removeClass('hidden');
+            else unprefixedNode.addClass('hidden');
+
+            if (runOpcodeSearch(e.target.value, tables.CBPrefixed[i], i)) CBPrefixedNode.removeClass('hidden');
+            else CBPrefixedNode.addClass('hidden');
+        }
+    })
+
+    // Don't use `getJson()` because it complains locally about the mimetype.
+    $.ajax({
+        dataType: "json",
+        url: "dmgops.json",
+        data: null,
+        mimeType: "application/json",
+        success: ops => {
+            tables = ops;
+            redrawTables();
+        }
+    })
 })
